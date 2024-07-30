2a0c4304c1f967edd2d36208790597f52cd38f8d
silvynrr <141999356+silvynrr@users.noreply.github.com>
2024-07-30 14:50:58 +0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="content">
+    <div class="container-fluid">
+      <div class="row mx-1 py-5">
+        <div class="col-lg-6 col mb-3">
+          <nuxt-link to="/pengunjung/riwayat" class="text-black text-decoration-none">
+            <div class="card bg-pengunjung rounded-5">
+              <div class="card-body pt-5">
+                <h2 class="text-center text-white mt-5">RIWAYAT KUNJUNGAN</h2>
+              </div>
+            </div>
+          </nuxt-link>
+        </div>
+
+        <div class="col-lg-6 col">
+          <nuxt-link to="/buku" class="text-black text-decoration-none">
+            <div class="card bg-buku rounded-5">
+              <div class="card-body pt-5">
+                <h2 class="text-center text-white mt-5">CARI BUKU</h2>
+              </div>
+            </div>
+          </nuxt-link>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <h2>Statistik</h2>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col col-lg-6">
+          <div class="card pengunjung rounded-5">
+            <div class="card-body">
+              <h2 class="text-center mt-5">{{ jmlPengunjung }} pengunjung</h2>
+            </div>
+          </div>
+        </div>
+        <div class="col col-lg-6">
+          <div class="card buku rounded-5">
+            <div class="card-body">
+              <h2 class="text-center mt-5">{{ jmlBuku }}Buku</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const supabase = useSupabaseClient()
+const jmlPengunjung = ref(0)
+const jmlBuku = ref(0)
+
+async function getJmlPengunjung() {
+  const { data, count } = await supabase.from("Pengunjung")
+    .select('*', { count: "exact" })
+  if (data) jmlPengunjung.value = count
+}
+
+async function getJmlBuku() {
+  const { data, count } = await supabase.from("Buku")
+    .select('*', { count: "exact" })
+  if (data) jmlBuku.value = count
+}
+onMounted(() => {
+  getJmlPengunjung()
+  getJmlBuku()
+})
+</script>
+<style scoped>
+.content {
+  background-color: #ffffff;
+  /* height: 100vh; */
+  width: 100%;
+}
+
+.card {
+  opacity: 85%;
+  align-items: center;
+  height: 250px;
+}
+
+.card.bg-pengunjung {
+  background-image: url('@/assets/img/bg-home-kunjungan.jpg');
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+}
+
+.card.bg-buku {
+  background: url('@/assets/img/bg-home-cari-buku.jpg');
+  background-size: cover;
+}
+
+.card.pengunjung {
+  /* warna card statistik pengunjung */
+  background-color: #ffff00;
+}
+
+.card.buku {
+  /* warna card statistik buku */
+  background-color: #6da2ac;
+}
+</style>
---
+++ pages/pengunjung/riwayat.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-12">
+        <h2 class="text-center my-4">RIWAYAT KUNJUNGAN</h2>
+        <div class="my-3">menampilkan 1 dari 1</div>
+        <table class="table table-bordered border-white text-white">
+          <thead>
+            <tr>
+              <td>#</td>
+              <td>Tanggal</td>
+              <td>Waktu</td>
+              <td>Nama</td>
+              <td>Keanggotaan</td>
+              <td>Kelas</td>
+              <td>Keperluan</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(visitor, i) in visitors" :key="i">
+              <td>{{ i+1 }}.</td>
+              <td>{{ visitor.tanggal }}</td>
+              <td>{{ visitor.waktu }}</td>
+              <td>{{ visitor.nama }}</td>
+              <td>{{ visitor.keanggotaan.nama }}</td>
+              <td>{{ visitor.tingkat}}-{{ visitor.jurusan }}{{ visitor.kelas }}</td>
+              <td>{{ visitor.keperluan.nama }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <div class="row float-end">
+        <nuxt-link to="/pengunjung" class="btn btn-dark btn-lg rounded-5 px-5">menu</nuxt-link>
+    </div>
+  </div>
+</template>
+<script setup>
+const supabase = useSupabaseClient()
+
+const visitors = ref([])
+const getPengunjung = async () => {
+  const { data, error } = await supabase.from('Pengunjung').select(`*, keanggotaan(*), keperluan(*)`)
+  if (error) throw error
+  if(data) visitors.value = data 
+}
+onMounted(() => {
+  getPengunjung()
+})
+</script>
+
+<style scoped>
+.container-fluid {
+  background: #235C4E;
+  background-size: cover;
+  height: 100vh;
+  width: 100%;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  color:rgb(0, 0, 0);
+  color: rgb(255, 255, 255);
+  opacity: 70%;
+}
+.btn{
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  background-color: rgb(243, 243, 243);
+  color: rgb(0, 0, 0);
+  width: 150px;
+  height: 50px;
+}
+
+.table td{
+  background: #235C4E;
+  color: black;
+}
+</style>
